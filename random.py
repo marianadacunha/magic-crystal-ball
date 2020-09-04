17e250d4be329c35edd441546874d780ad7ca719
Mariana Marques <marianadacunhamarques@gmail.com>
2020-09-04 14:29:44 -0300

    setting up flask

---
+++ random.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import random
+
+# Initializing a variable of Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    number = random.randint(0, 8)
+    return render_template("index.html", number=number)
